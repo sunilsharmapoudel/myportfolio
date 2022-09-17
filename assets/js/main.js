0daca484f349f338e2fe5e4e14b00a6e31b679b2
Sunil Sharma <sunilpudel1999@gmail.com>
2022-09-17 11:17:51 +0545

    portfolio finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,11 +32,36 @@ let mixerPortfolio = mixitup('.work_container', {
 
 /* -- Link Active Work --*/
 
+
+/* 
 const linkWork = document.querySelectorAll('.work_item');
 
 function activeWork() {
-    linkWork.forEach(L=> IDBCursor.classList.remove('active-work'))
+    linkWork.forEach(l=> I.Cursor.classList.remove('active-work'))
     this.classList.add('active-work')
 }
 
-linkWork.forEach(L=> L.addEventListener("click", activeWork))
+linkWork.forEach(l=> I.addEventListener("click", activeWork));
+ */
+
+
+/*-- Work Popup --*/
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work_button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio_popup-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp_thumbnail img").src = portfolioItem.querySelector(".work_img").src;
+    document.querySelector(".portfolio_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;
+    document.querySelector(".portfolio_popup-body").innerHTML = portfolioItem.querySelector(".portfolio_item-details").innerHTML;
+
+}
