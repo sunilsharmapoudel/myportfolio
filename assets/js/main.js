92735633217780c0002cdb3c07a1e4829bc236ac
Sunil Sharma <sunilpudel1999@gmail.com>
2022-10-14 14:04:17 +0545

    JQuery Finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
+/* -- SHOW SIDEBAR --*/
+const navMenu = document.getElementById('sidebar'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+/* Sidebar Show */
+/* Validate if const exists */
+
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-sidebar')
+    })
+}
+
+/* Sidebar Hidden */
+/**Validate if consts exists */
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-sidebar')
+    })
+}
+
 /* Skills tabs */
 const tabs = document.querySelectorAll('[data-target]'),
     tabContent = document.querySelectorAll('[data-content]')
@@ -47,7 +69,7 @@ linkWork.forEach(l=> I.addEventListener("click", activeWork));
 
 /*-- Work Popup --*/
 document.addEventListener("click", (e) => {
-    if(e.target.classList.contains("work_button")) {
+    if (e.target.classList.contains("work_button")) {
         togglePortfolioPopup();
         portfolioItemDetails(e.target.parentElement);
     }
@@ -68,21 +90,21 @@ function portfolioItemDetails(portfolioItem) {
 
 // Services Modal //
 const modalViews = document.querySelectorAll('.services_modal'),
-modalBtns = document.querySelectorAll('.services_button'),
-modalCloses = document.querySelectorAll('.services_modal-close')
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalCloses = document.querySelectorAll('.services_modal-close')
 
-let modal = function(modalClick) {
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal');
 }
 
 modalBtns.forEach((modelBtn, i) => {
-    modelBtn.addEventListener('click', ()=> {
+    modelBtn.addEventListener('click', () => {
         modal(i)
     })
 })
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener("click", () =>{
+    modalClose.addEventListener("click", () => {
         modalViews.forEach((modalViews) => {
             modalViews.classList.remove('active-modal')
         })
@@ -91,21 +113,71 @@ modalCloses.forEach((modalClose) => {
 
 // Swiper Testnimonial 
 let swiper = new Swiper(".testimonials_container", {
-  spaceBetween: 25,
-  loop: true,
-  grabCursor: true,
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
-      
+    spaceBetween: 25,
+    loop: true,
+    grabCursor: true,
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 });
+
+
+// Input Animation
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+
+})
+
+//get all sections that ahve an id defined
+const sections = document.querySelectorAll("section[id]");
+
+//add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+
+    //get current scroll position
+    let scrollY = window.pageYOffset;
+    //now we loop throught sections to get height, top and ID values for each
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+            sectionID = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionID + ']').classList.add("active-link")
+        }
+        else {
+            document.querySelector('.nav_menu a[href*=' + sectionID + ']').classList.remove("active-link")
+        }
+    })
+}
